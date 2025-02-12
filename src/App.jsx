82ee055a6e8faa8c03deddb3bd227ed95404abe9
Olivier <Olsnyers@hotmail.com>
2025-02-12 21:37:55 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 import HomePage from "./HomePage.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <HomePage />
-    </div>
-  );
-}
+
 
 function App() {
   return (
