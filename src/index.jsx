df8023735895f9e70562bcddf3c93d88cad170bf
Olivier <Olsnyers@hotmail.com>
2025-02-12 21:39:34 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -11,15 +11,6 @@ root.render(
   </React.StrictMode>
 );
 
-function App() {
-  return (
-    <div style={{ fontFamily: 'Schoensperger Fr, sans-serif' }}>
-      <h1>Test de la police personnalisée</h1>
-      <p>Si ce texte s'affiche dans la bonne police, c'est bon !</p>
-    </div>
-  );
-}
-export default App;
 
 
 // If you want to start measuring performance in your app, pass a function
